5e79446d73bd06bf16c594fc5acef0bc4eaccab2
vanochet <rusllikccx@gmail.com>
2024-03-27 17:08:07 +0200

    Changet standart size

--- ConsoleApp35/Program.cs
+++ ConsoleApp35/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            using (Window game = new Window(600, 600, "rabotai"))
+            using (Window game = new Window(800, 600, "rabotai"))
             {
                 game.Run();
             }
