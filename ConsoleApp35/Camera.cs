f54cfc01874ef9f9b8e3e618df2883317b649d08
vanochet <rusllikccx@gmail.com>
2024-03-27 17:05:15 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp35/Camera.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Mathematics;
+using OpenTK.Graphics.OpenGL;
+
+
+namespace ConsoleApp35
+{
+    public class Camera
+    {
+        public Vector2 position;
+        public double rotation, zoom;
+        private float width, height, ratio;
+        private Matrix4 sizeProjection = Matrix4.Identity;
+        public Camera(Vector2 position, double rotation, double zoom, float width, float height)
+        {
+            this.position = position;
+            this.rotation = rotation;
+            this.zoom = zoom;
+            this.width = width;
+            this.height = height;
+            ratio = width / height;
+            sizeProjection = Matrix4.CreateOrthographic(width , height , 0f, 0f);
+        }
+
+        
+        public Camera(Camera other)
+        {
+            this.position = new Vector2(other.position.X, other.position.Y);
+            this.rotation = other.rotation;
+            this.zoom = other.zoom;
+            this.width = other.width;
+            this.height = other.height;
+            ratio = width / height;
+            sizeProjection = Matrix4.CreateOrthographicOffCenter(-0.99f, 1, -0.99f, 1, -1, 1);
+
+
+        }
+
+        public void UpdateSize(float width, float height)
+        {
+            this.width = width;
+            this.height = height;
+            ratio = width / height;
+            sizeProjection = Matrix4.CreateOrthographic(width * ratio, height * ratio, 0f, 0f);
+
+        }
+
+        public Matrix4 GetTransform()
+        {
+
+            Matrix4 transform = Matrix4.Identity;
+            transform = Matrix4.Mult(transform, Matrix4.CreateTranslation(-position.X, -position.Y, 0));
+            transform = Matrix4.Mult(transform, Matrix4.CreateRotationZ(-(float)rotation));
+            transform = Matrix4.Mult(transform, Matrix4.CreateScale((float)zoom, (float)zoom, 1.0f));
+
+
+            return transform;
+        }
+    }
+}
---
+++ ConsoleApp35/Window.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+// GL.ClearColor(new Color4(0.3f, 0.4f, 0.5f, 1f));
+//        public Window(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title }) { }
+// 
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using OpenTK.Mathematics;
+using ConsoleApp35;
+namespace ConsoleApp34
+{
+
+
+
+    public class Shader
+    {
+        public readonly int Handle;
+
+        private readonly Dictionary<string, int> _uniformLocations;
+
+        public Shader(string vertPath, string fragPath)
+        {
+
+            var shaderSource = File.ReadAllText(vertPath);
+
+
+            var vertexShader = GL.CreateShader(ShaderType.VertexShader);
+
+
+            GL.ShaderSource(vertexShader, shaderSource);
+
+
+            CompileShader(vertexShader);
+
+            shaderSource = File.ReadAllText(fragPath);
+            var fragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(fragmentShader, shaderSource);
+            CompileShader(fragmentShader);
+
+
+            Handle = GL.CreateProgram();
+
+
+            GL.AttachShader(Handle, vertexShader);
+            GL.AttachShader(Handle, fragmentShader);
+
+
+            LinkProgram(Handle);
+
+
+            GL.DetachShader(Handle, vertexShader);
+            GL.DetachShader(Handle, fragmentShader);
+            GL.DeleteShader(fragmentShader);
+            GL.DeleteShader(vertexShader);
+
+
+            GL.GetProgram(Handle, GetProgramParameterName.ActiveUniforms, out var numberOfUniforms);
+
+
+            _uniformLocations = new Dictionary<string, int>();
+
+
+            for (var i = 0; i < numberOfUniforms; i++)
+            {
+
+                var key = GL.GetActiveUniform(Handle, i, out _, out _);
+
+                var location = GL.GetUniformLocation(Handle, key);
+
+
+                _uniformLocations.Add(key, location);
+            }
+        }
+
+        private static void CompileShader(int shader)
+        {
+
+            GL.CompileShader(shader);
+
+
+            GL.GetShader(shader, ShaderParameter.CompileStatus, out var code);
+            if (code != (int)All.True)
+            {
+
+                var infoLog = GL.GetShaderInfoLog(shader);
+                throw new Exception($"Error occurred whilst compiling Shader({shader}).\n\n{infoLog}");
+            }
+        }
+
+        private static void LinkProgram(int program)
+        {
+
+            GL.LinkProgram(program);
+
+
+            GL.GetProgram(program, GetProgramParameterName.LinkStatus, out var code);
+            if (code != (int)All.True)
+            {
+
+                throw new Exception($"Error occurred whilst linking Program({program})");
+            }
+        }
+
+
+        public void Use()
+        {
+            GL.UseProgram(Handle);
+        }
+
+
+        public int GetAttribLocation(string attribName)
+        {
+            return GL.GetAttribLocation(Handle, attribName);
+        }
+
+
+        public void SetInt(string name, int data)
+        {
+            GL.UseProgram(Handle);
+            GL.Uniform1(_uniformLocations[name], data);
+        }
+
+
+        public void SetFloat(string name, float data)
+        {
+            GL.UseProgram(Handle);
+            GL.Uniform1(_uniformLocations[name], data);
+        }
+
+
+        public void SetMatrix4(string name, Matrix4 data)
+        {
+            GL.UseProgram(Handle);
+            GL.UniformMatrix4(_uniformLocations[name], true, ref data);
+        }
+
+
+        public void SetVector3(string name, Vector3 data)
+        {
+            GL.UseProgram(Handle);
+            GL.Uniform3(_uniformLocations[name], data);
+        }
+    }
+
+    public class Window : GameWindow
+    {
+        private readonly Camera _camera;
+        private readonly float[] _vertices =
+        {
+            // Position         Texture coordinates
+             0.5f,  0.5f, 0.0f, 1.0f, 1.0f, // top right
+             0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // bottom right
+            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, // bottom left
+            -0.5f,  0.5f, 0.0f, 0.0f, 1.0f  // top left
+        };
+
+        private readonly uint[] _indices =
+        {
+            0, 1, 3,
+            1, 2, 3
+        };
+
+        private int _elementBufferObject;
+
+        private int _vertexBufferObject;
+
+        private int _vertexArrayObject;
+
+        private Shader _shader;
+
+
+        private Texture _texture;
+
+        private bool isHolding = false;
+
+        public Window(int width, int height, string title) : 
+            base(GameWindowSettings.Default, new NativeWindowSettings() { ClientSize = (width, height), Title = title }) 
+        { 
+            _camera = new Camera(Vector2.Zero, 0.0,1.0, Size.X,Size.Y);
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+            GL.Enable(EnableCap.Blend);
+            GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
+            GL.Enable(EnableCap.DepthTest);
+            GL.ClearColor(new Color4(0.3f, 0.4f, 0.5f, 1f));
+            _vertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(_vertexArrayObject);
+
+            _vertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+
+            _elementBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, _indices.Length * sizeof(uint), _indices, BufferUsageHint.StaticDraw);
+
+
+            _shader = new Shader("Shaders/shader.vert", "Shaders/shader.frag");
+            _shader.Use();
+
+
+            var vertexLocation = _shader.GetAttribLocation("aPosition");
+            GL.EnableVertexAttribArray(vertexLocation);
+            GL.VertexAttribPointer(vertexLocation, 3, VertexAttribPointerType.Float, false, 5 * sizeof(float), 0);
+
+
+            var texCoordLocation = _shader.GetAttribLocation("aTexCoord");
+            GL.EnableVertexAttribArray(texCoordLocation);
+            GL.VertexAttribPointer(texCoordLocation, 2, VertexAttribPointerType.Float, false, 5 * sizeof(float), 3 * sizeof(float));
+
+            _texture = TexturePipe.Load("Resources/container.png");
+            _texture.Use(TextureUnit.Texture0);
+
+            
+
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+/*            GL.LoadIdentity();
+            GL.Ortho(-this.ClientSize.X / 2f, this.ClientSize.X / 2f, this.ClientSize.Y / 2f, -this.ClientSize.Y / 2, 0.0f, 1f);*/
+            _shader.SetMatrix4("projection", _camera.GetTransform());
+
+
+            
+            GL.BindVertexArray(_vertexArrayObject);
+
+            _texture.Use(TextureUnit.Texture0);
+            _shader.Use();
+
+            GL.DrawElements(PrimitiveType.Triangles, _indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            SwapBuffers();
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+            //camera.position.X += 0.1f;
+            var input = KeyboardState;
+
+
+        }
+
+        protected override void OnKeyDown(KeyboardKeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+            switch(e.Key)
+            {
+                case Keys.Escape:
+                    Close();
+                    break;
+                case Keys.Down:
+                    _camera.position.Y += 0.1f;
+                    break;
+                case Keys.Up:
+                    _camera.position.Y -= 0.1f;
+                    break;
+                case Keys.Left:
+                    _camera.position.X += 0.1f;
+                    break;
+                case Keys.Right:
+                    _camera.position.X -= 0.1f;
+                    break;
+                case Keys.Q:
+                    _camera.rotation -= double.Pi / 64;
+                    break;
+                case Keys.E:
+                    _camera.rotation += double.Pi / 64;
+                    break;
+            }
+        }
+
+        protected override void OnMouseWheel(MouseWheelEventArgs e)
+        {
+            base.OnMouseWheel(e);
+            switch (e.Offset[1])
+            {
+                case 1:
+                    _camera.zoom += 0.1;
+                    break;
+                case -1:
+                    _camera.zoom -= 0.1;
+                    break;
+            }
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(0, 0, Size.X, Size.Y);
+        }
+    }
+}
